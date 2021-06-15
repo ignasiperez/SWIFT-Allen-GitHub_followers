5b81275f724cd85afd2514aa6e121673e296f44d
Ignasi  Perez <perezignasi@gmail.com>
2021-06-15 09:08:31 +0200

    SW-V03-Step02-GHFollowers

---
+++ SW-V03-Step02-GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by ignasiperez.com on 15/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemPink
+  }
+
+}
+
