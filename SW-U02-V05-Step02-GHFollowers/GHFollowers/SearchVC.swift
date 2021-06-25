64b0e2c34ac7422eec6710536493d19a4c0ef4cf
Ignasi  Perez <perezignasi@gmail.com>
2021-06-25 19:02:56 +0200

    SW-U02-V05-Step02-GHFollowers

---
+++ SW-U02-V05-Step02-GHFollowers/GHFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by ignasiperez.com on 22/06/2021.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemPink
+  }
+  
+}
