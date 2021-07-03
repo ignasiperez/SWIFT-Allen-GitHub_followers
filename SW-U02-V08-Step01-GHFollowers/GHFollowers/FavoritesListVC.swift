da3f28e58c93c9d1cc3c3a873f21e0c95f9a70c3
Ignasi  Perez <perezignasi@gmail.com>
2021-07-03 08:01:53 +0200

    SW-U02-V08-Step01-GHFollowers

---
+++ SW-U02-V08-Step01-GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteListVC.swift
+//  GHFollowers
+//
+//  Created by ignasiperez.com on 22/06/2021.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemBlue
+  }
+  
+}
